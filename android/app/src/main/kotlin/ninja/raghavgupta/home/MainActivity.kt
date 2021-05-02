382f26be8abf6440f7adbfc8d29a159e9d54f81a
raghav gupta <raghavguptaeng@users.noreply.github.com>
2021-05-02 20:32:29 +0530

    Remaining resources added

---
+++ android/app/src/main/kotlin/ninja/raghavgupta/home/MainActivity.kt
@@ -0,0 +1,6 @@
+package ninja.raghavgupta.home
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,1 @@
+String kBackgroundImage = 'https://images.unsplash.com/photo-1599696848652-f0ff23bc911f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bGl2aW5nJTIwcm9vbXxlbnwwfDF8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60';
